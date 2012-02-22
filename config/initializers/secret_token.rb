1597e3aa972587077f6974af59aae8a62e91059b
Elvis Wu <elvis.wu21@gmail.com>
2012-02-22 18:34:51 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElvisBlog::Application.config.secret_token = '6dc46ba8cf5c2b8204c64344f5817e564b6eb3b9905e619c35b5538dc9b18725b51bfec26d9d247495e3fb9a9d035583b618e54df577fc40fccbaacc2044ca66'
